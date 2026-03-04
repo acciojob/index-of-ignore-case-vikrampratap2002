771a6ca1a1ca10ea7a4e4b5fdc69fdc52677fdf7
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-04 22:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function indexOfIgnoreCase(s1, s2) {
+function indexOfIgnoreCase(str, subStr) {
   // write your code here
+	    if (subStr === "") return 0;
+
+    str = str.toLowerCase();
+    subStr = subStr.toLowerCase();
+
+    for (let i = 0; i <= str.length - subStr.length; i++) {
+        if (str.substring(i, i + subStr.length) === subStr) {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
-// Please do not change the code below
+// Please do not change the code below 
+
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
